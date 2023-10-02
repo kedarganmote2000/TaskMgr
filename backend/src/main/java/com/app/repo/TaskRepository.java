d16847fa84b0bd702ded0bd85f7616fa9915b0fc
Kedar_Ganmote <kedarganmote2000@gmail.com>
2023-10-02 18:23:24 +0530

    Update TaskRepository.java

--- backend/src/main/java/com/app/repo/TaskRepository.java
+++ backend/src/main/java/com/app/repo/TaskRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.app.Entity.Task;
 
+//JpaRepository Class is Extended which contains method such as (save(), findAll(), findbyID().
 public interface TaskRepository extends JpaRepository<Task, Long>{
 
 }
