cea6f5ff611fccf739ef077b0c2cdd1dfe4b65f4
Kedar_Ganmote <kedarganmote2000@gmail.com>
2023-10-02 18:14:14 +0530

    Update TaskController.java

--- backend/src/main/java/com/app/controller/TaskController.java
+++ backend/src/main/java/com/app/controller/TaskController.java
@@ -17,29 +17,39 @@ import com.app.exception.TaskNotFoundException;
 import com.app.repo.*;
 
 
-
+//This is the TaskController Class here various request method (such as post, get, put etc) get executed.
 @RestController
+//@RestController : It simply return the object of a class and the object data is directly written in HTTP response as JSON.
 @CrossOrigin("http://localhost:3000")
+//@CrossOrigin : It's used to establish a Connection between Frontend and Backend.	
+	
 public class TaskController {
 
+	//@AutoWired : It's used to automatically wire dependencies into a spring bean.
 	@Autowired
 	private TaskRepository TaskRepo; 
-	
+
+	//@PostMapping : It's used to get the Post request Map to this method.
 	@PostMapping("/task")
 	Task newUser(@RequestBody Task newTask) {
 		System.out.println(newTask.toString());
 		return TaskRepo.save(newTask);
+		//save() is JPA Repo Method which is used to save the details.
 	}
-	
+
+	//@GetMapping : It's used to get the Get request Map to this method.
 	@GetMapping("/taskdetails")
 	List<Task> getalluser(){
 		return TaskRepo.findAll();
+		//findAll() is JPA Repo Method which is used to find the details.
 	}
 	@GetMapping("/task/{id}") 
 	Task getUserbyId(@PathVariable Long id) {
 		return TaskRepo.findById(id)
 				.orElseThrow(()-> new TaskNotFoundException(id)); 
+		//findById() is JPA Repo Method which is used to find the details by ID.
 	}
+	//@PutMapping : It's used to update the task.
 	@PutMapping("/task/{id}")
 	Task updateUser(@RequestBody Task newTask,@PathVariable Long id) {
 		return TaskRepo.findById(id)
@@ -50,6 +60,7 @@ public class TaskController {
 			    	 return TaskRepo.save(task);
 			    }).orElseThrow(() -> new TaskNotFoundException(id));
 	}
+	//@DeleteMapping : It's used to Delete the task.
 	@DeleteMapping("/task/{id}")
 		String deleteUser(@PathVariable Long id) {
 			if(!TaskRepo.existsById(id)) {
@@ -57,6 +68,7 @@ public class TaskController {
 			}
 			TaskRepo.deleteById(id);
 			return "Task with id "+id+" has been Deleted.";
+			//deleteById() is JPA Repo Method which is used to delete the details by ID.
 		}
 	}
 
