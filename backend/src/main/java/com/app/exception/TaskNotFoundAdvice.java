7239d8a0d3bb489e0b98d210b525addda61b2436
Kedar_Ganmote <kedarganmote2000@gmail.com>
2023-10-02 18:20:26 +0530

    Update TaskNotFoundAdvice.java

--- backend/src/main/java/com/app/exception/TaskNotFoundAdvice.java
+++ backend/src/main/java/com/app/exception/TaskNotFoundAdvice.java
@@ -10,12 +10,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+	//@ControllerAdvice : It's used in Spring to define a class that handles exceptions globally across all controllers
 public class TaskNotFoundAdvice {
-
 	
 	@ResponseBody
+	//@Response : Tells a controller that the object returned is automatically serialized into JSON and passed back into the HttpResponse object
 	@ExceptionHandler(TaskNotFoundException.class)
+	//@ExceptionHandler : It's used in Spring Boot to handle specific exceptions and send custom responses to the client.
 	@ResponseStatus(HttpStatus.NOT_FOUND)
+	//@ResponseStatus : It's used to mark a method or exception class with a status code and reason that should be returned.
 	public Map<String,String> exceptionHandler(TaskNotFoundException exception){
 		
 		Map<String,String> errorMap=new HashMap<>(); 
