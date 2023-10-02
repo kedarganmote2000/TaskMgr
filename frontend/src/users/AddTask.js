87765df7896a7186c46ce16bb6fe7f1b95cdcbcb
Kedar_Ganmote <kedarganmote2000@gmail.com>
2023-10-02 18:50:26 +0530

    Update AddTask.js

--- frontend/src/users/AddTask.js
+++ frontend/src/users/AddTask.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+//In this Component User can Add the NewTask
 export default function AddTask() {
   let navigate = useNavigate();  //It's used to Navigate to the Home Page.
 
